65efbeb3b19738fb87777748bab6064e4440cf8c
Mustaf3654 <maboualhajj@gmail.com>
2026-08-13 15:13:47 +0300

    Remove supplier and account admin sections

--- app/Filament/Resources/Accounts/Pages/EditAccount.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Filament\Resources\Accounts\Pages;
-use App\Filament\Resources\Accounts\AccountResource;
-use Filament\Resources\Pages\EditRecord;
-class EditAccount extends EditRecord { protected static string $resource = AccountResource::class; }
--- app/Filament/Resources/Suppliers/Pages/EditSupplier.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Filament\Resources\Suppliers\Pages;
-use App\Filament\Resources\Suppliers\SupplierResource;
-use Filament\Resources\Pages\EditRecord;
-class EditSupplier extends EditRecord { protected static string $resource = SupplierResource::class; }

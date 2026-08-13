14543bbb120d6ba6c1525dc55b2c571536af28e4
Mustaf3654 <maboualhajj@gmail.com>
2026-08-13 14:26:29 +0300

    Post purchase invoice account balances

--- app/Filament/Resources/PurchaseInvoices/Pages/CreatePurchaseInvoice.php
+++ app/Filament/Resources/PurchaseInvoices/Pages/CreatePurchaseInvoice.php
@@ -32,6 +32,11 @@ class CreatePurchaseInvoice extends CreateRecord {
 
     protected function afterCreate(): void
     {
+        $amount = (float) $this->record->total;
+
+        \App\Models\Account::whereKey($this->record->debit_account_id)->increment('balance', $amount);
+        \App\Models\Account::whereKey($this->record->credit_account_id)->increment('balance', $amount);
+
         foreach ($this->invoiceItems as $item) {
             \App\Models\ProductVariant::whereKey($item['product_variant_id'])->update([
                 'cost_price' => (float) $item['unit_cost'],

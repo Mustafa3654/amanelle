492146771c28095f7f8293a793b648151b337c15
Mustaf3654 <maboualhajj@gmail.com>
2026-08-13 16:46:56 +0300

    Make purchase history migration safe

--- database/migrations/2026_08_13_100031_allow_product_deletion_with_purchase_history.php
+++ database/migrations/2026_08_13_100031_allow_product_deletion_with_purchase_history.php
@@ -3,14 +3,23 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('purchase_invoice_items', function (Blueprint $table) {
-            $table->dropForeign(['product_variant_id']);
-        });
+        $constraint = DB::table('information_schema.REFERENTIAL_CONSTRAINTS')
+            ->where('CONSTRAINT_SCHEMA', DB::getDatabaseName())
+            ->where('TABLE_NAME', 'purchase_invoice_items')
+            ->where('CONSTRAINT_NAME', 'purchase_invoice_items_product_variant_id_foreign')
+            ->exists();
+
+        if ($constraint) {
+            Schema::table('purchase_invoice_items', function (Blueprint $table) {
+                $table->dropForeign(['product_variant_id']);
+            });
+        }
 
         Schema::table('purchase_invoice_items', function (Blueprint $table) {
             $table->foreign('product_variant_id')

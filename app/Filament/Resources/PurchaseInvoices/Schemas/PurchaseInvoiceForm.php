26c4fee0434b934079afc84d8e13a3cd9d703b0e
Mustaf3654 <maboualhajj@gmail.com>
2026-08-13 14:30:59 +0300

    Fix invoice search field import

--- app/Filament/Resources/PurchaseInvoices/Schemas/PurchaseInvoiceForm.php
+++ app/Filament/Resources/PurchaseInvoices/Schemas/PurchaseInvoiceForm.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\PurchaseInvoices\Schemas;
 
 use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Repeater;
